aeccc9fc5cc916b9194a62df492f98eb812a3d03
Ritik Raina <rraina@ucsd.edu>
2019-07-24 13:29:20 +0700

    Update face_recog.py

--- face_recog.py
+++ face_recog.py
@@ -11,6 +11,7 @@ import imutils
 import time
 import dlib
 import cv2
+import face_recognition
 
 facePath = '/Users/rraina/Desktop/INTERNSHIP 2019/FacialReg/haarcascade_frontalface_alt.xml'
 eyePath = '/Users/rraina/Desktop/INTERNSHIP 2019/FacialReg/haarcascade_lefteye_2splits.xml'
@@ -20,11 +21,20 @@ eyeCascade = cv2.CascadeClassifier(eyePath)
 
 video_capture = cv2.VideoCapture(0)
 
+known_face_encodings = []
+known_face_names = []
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
+    
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(small_frame, cv2.COLOR_BGR2GRAY)
 
     faces = faceCascade.detectMultiScale(
         gray,
@@ -36,25 +46,19 @@ while True:
 
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h+12), (0, 255, 0), 2)
-        eyes = eyeCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.2,
-            minNeighbors=5,
-            minSize=(10, 10)
-        )
-        for (x, y, w, h) in eyes:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 0), 2)
+        img = cv2.rectangle(small_frame, (x, y), (x+w, y+h), (255, 0, 0), 1)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(img, "Ritik", (x, y-10), font, 0.7, (100,255,100), 1)
 
 
     # Display the resulting frame
-    cv2.imshow('Video', frame)
+    cv2.imshow('Video', small_frame)
     path = '/Users/rraina/Desktop/INTERNSHIP 2019/FacialReg/faces'
     os.chdir(path)
     total = len(faces)
     print("Faces Detected: " + str(total))
     for n in range(0, total):
-        img = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        img = cv2.rectangle(small_frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
         face = img[y:faces[n][1] + faces[n][3], x:faces[n][0] + faces[n][2]]
         name = "face" + str(n) + ".jpg"
         cv2.imwrite(name, face)
