2d7d6117be1d8b7be74f9b04956fbf5f158f83c2
Ritik Raina <rraina@ucsd.edu>
2019-07-25 10:53:13 +0700

    Update face_recog.py

--- face_recog.py
+++ face_recog.py
@@ -5,6 +5,7 @@ from scipy.spatial import distance as dist
 from imutils.video import FileVideoStream
 from imutils.video import VideoStream
 from imutils import face_utils
+from matplotlib import pyplot as plt
 import numpy as np
 import argparse
 import imutils
@@ -12,6 +13,9 @@ import time
 import dlib
 import cv2
 import face_recognition
+import img
+from pygame import mixer
+from gtts import gTTS 
 
 facePath = '/Users/rraina/Desktop/INTERNSHIP 2019/FacialReg/haarcascade_frontalface_alt.xml'
 eyePath = '/Users/rraina/Desktop/INTERNSHIP 2019/FacialReg/haarcascade_lefteye_2splits.xml'
@@ -28,11 +32,13 @@ face_encodings = []
 face_names = []
 process_this_frame = True
 
+Ritik_came = False
+
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
     
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    small_frame = cv2.resize(frame, (0, 0), fx=1.00, fy=1.00)
 
     gray = cv2.cvtColor(small_frame, cv2.COLOR_BGR2GRAY)
 
@@ -47,22 +53,30 @@ while True:
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         img = cv2.rectangle(small_frame, (x, y), (x+w, y+h), (255, 0, 0), 1)
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(img, "Ritik", (x, y-10), font, 0.7, (100,255,100), 1)
 
 
     # Display the resulting frame
     cv2.imshow('Video', small_frame)
-    path = '/Users/rraina/Desktop/INTERNSHIP 2019/FacialReg/faces'
-    os.chdir(path)
-    total = len(faces)
-    print("Faces Detected: " + str(total))
-    for n in range(0, total):
-        img = cv2.rectangle(small_frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        face = img[y:faces[n][1] + faces[n][3], x:faces[n][0] + faces[n][2]]
-        name = "face" + str(n) + ".jpg"
-        cv2.imwrite(name, face)
-
+    
+    for n in range(0, len(faces)):
+        image1 = img[y:faces[n][1] + faces[n][3], x:faces[n][0] + faces[n][2]]
+        image2 = face_recognition.load_image_file("/Users/rraina/Desktop/INTERNSHIP 2019/FacialReg/Training Images/image.jpg") 
+        if (len(image1) > 0):
+            if(len(face_recognition.face_encodings(image1)) > 0):
+                encoding_1 = face_recognition.face_encodings(image1)[0]
+                encoding_2 = face_recognition.face_encodings(image2)[0]
+                results_Ritik = face_recognition.compare_faces([encoding_1], encoding_2,tolerance=0.70)
+                if ((results_Ritik[0] == True) and (Ritik_came == False)):
+                    Ritik_came = True
+                    mytext = 'Welcome Mr. Ritik Raina'
+                    language = 'en'
+                    myobj = gTTS(text=mytext, lang=language, slow=False)
+                    myobj.save("welcome.mp3")
+                    mixer.init()
+                    mixer.music.load('/Users/rraina/Desktop/INTERNSHIP 2019/FacialReg/welcome.mp3')
+                    mixer.music.play()
+                    said = False
+    said = True            
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
